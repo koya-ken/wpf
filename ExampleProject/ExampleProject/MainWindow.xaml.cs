3e971aec7aa366e4a66c844496b808807e7d89f4
koya-ken <koya.smp@gmail.com>
2025-03-16 03:07:11 +0900

    update

--- ExampleProject/ExampleProject/MainWindow.xaml.cs
+++ ExampleProject/ExampleProject/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using ExampleProject.src.View;
+using ExampleProject.View;
 using System.Windows;
+using System.Windows.Media;
 
 namespace ExampleProject;
 
@@ -8,9 +10,24 @@ namespace ExampleProject;
 /// </summary>
 public partial class MainWindow : Window
 {
+    EditableRectangle Child1;
     public MainWindow()
     {
         InitializeComponent();
+
+        Child1 = new EditableRectangle { Width = 200, Height = 100, Background = Brushes.Red };
+        MainCanvas.Children.Add(Child1);
+        //Start();
+    }
+
+    async Task Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+
+            await Task.Delay(TimeSpan.FromMilliseconds(33));
+            Child1.X += 2;
+        }
     }
 
     private void Button_Click(object sender, RoutedEventArgs e)
---
+++ ExampleProject/ExampleProject/View/EditableRectangle.xaml.cs
@@ -0,0 +1,104 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ExampleProject.View
+{
+    /// <summary>
+    /// ObservableRectangle.xaml の相互作用ロジック
+    /// </summary>
+    [INotifyPropertyChanged]
+    public partial class EditableRectangle : UserControl
+    {
+        [ObservableProperty]
+        public double _x;
+        [ObservableProperty]
+        public double _y;
+
+        private double StartX;
+        private double StartY;
+
+        private Point DragStartPoint;
+        private Stack<Cursor> CursorStack = new(1);
+
+        public EditableRectangle()
+        {
+            InitializeComponent();
+        }
+
+        partial void OnXChanged(double oldValue, double newValue)
+        {
+            Canvas.SetLeft(this, newValue);
+        }
+
+        partial void OnYChanged(double oldValue, double newValue)
+        {
+            Canvas.SetTop(this, newValue);
+        }
+
+        private void OnMouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (this.Cursor != Cursors.SizeAll)
+            {
+                CursorStack.Push(Cursor);
+            }
+            this.Cursor = Cursors.SizeAll;
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            if (Mouse.Captured == this)
+            {
+                Mouse.Capture(null);
+            }
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            var windowActive = Application.Current.Windows.OfType<Window>().FirstOrDefault()?.IsActive;
+            if (windowActive != true)  // 親ウィンドウがアクティブ
+            {
+                return;
+            }
+            if (Mouse.Captured != this)
+            {
+                return;
+            }
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            // GetPositionしているときに対象の要素の位置を変えるとおかしくなるので
+            // 親からの位置にしている
+            var currentPosition = e.GetPosition((IInputElement)this.Parent);
+            var offset = currentPosition - DragStartPoint;
+            this.X = offset.X;
+            this.Y = offset.Y;
+            Debug.WriteLine($"{(e.RoutedEvent, e.Handled)} {(X, Y)} current:{currentPosition}, drag:{DragStartPoint}");
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine("Down Called");
+            if (Mouse.Captured == this)
+            {
+                return;
+            }
+            DragStartPoint = e.GetPosition(this);
+            StartX = X;
+            StartY = Y;
+            Mouse.Capture(this);
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.Captured != this)
+            {
+                return;
+            }
+            Mouse.Capture(null);
+        }
+    }
+}
